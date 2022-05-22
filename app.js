acdf4823e67326abe309b7dd26a9cfef9589b993
Vu_Huy <quochuy123dh@gmail.com>
2022-05-22 14:19:59 +0700

    work with file system
    work with folder

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-const tutorial = require('./tutorial')
-console.log(tutorial)
-console.log(tutorial.PI)
-console.log(tutorial.sum(1, 2))
-console.log(new tutorial.SomeMathObject())
+const fs = require('fs')
+
+//create a folder
+fs.mkdir('tutorial', (err) => {
+    if(err)
+        console.log(err)
+    else{
+        console.log('Folder successfully created')
+    }
+})
+
+//delete a folder
+fs.rmdir('tutorial', (err) => {
+    if(err)
+        console.log(err)
+    else{
+        console.log('folder successfully removed')
+    }
+})
+
+//delete a folder of multiple files (create a folder example contains multiple file to practice)
+fs.readdir('example', (err, files) => {
+    if(err)
+        console.log(err)
+    else{
+        for (let file of files){
+            fs.unlink('./example/' + file, (err) => {
+                if(err)
+                    console.log(err)
+                else{
+                    console.log('successfully deleted file')
+                }
+            })
+        }
+        fs.rmdir('example', (err) => {
+            if(err)
+                console.log(err)
+            else 
+                console.log('successfully')
+        })
+    }
+})
