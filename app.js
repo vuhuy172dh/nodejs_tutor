4caf7f5f9e96db8fdfe3d0163e6106d08a20d516
Vu_Huy <quochuy123dh@gmail.com>
2022-05-22 22:04:50 +0700

    pipe and pipe chaining
    practice pipe and pipe chaining

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const tutorial = require('./tutorial')
-console.log(tutorial)
-console.log(tutorial.PI)
-console.log(tutorial.sum(1, 2))
-console.log(new tutorial.SomeMathObject())
+const fs = require('fs')
+// const readStream = fs.createReadStream('./example.txt','utf-8')
+// const writeStream = fs.createWriteStream('example2.txt')
+
+//write file
+// readStream.pipe(writeStream)
+
+//compress file
+const zlib = require('zlib')
+// const gzip = zlib.createGzip()
+
+// const readStream = fs.createReadStream('./example.txt', 'utf-8')
+// const writeStream = fs.createWriteStream('compressed.zip')
+// readStream.pipe(gzip).pipe(writeStream)
+
+//uncompress file
+const gunzip = zlib.createGunzip()
+const readStream = fs.createReadStream('./compressed.zip')
+const writeStream = fs.createWriteStream('uncompressed.txt')
+readStream.pipe(gunzip).pipe(writeStream)
