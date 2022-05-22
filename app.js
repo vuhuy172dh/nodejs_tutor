7df1053d36a1117a223cc9cee4bb4260a2ea9262
Vu_Huy <quochuy123dh@gmail.com>
2022-05-22 23:00:24 +0700

    create HTTP server with HTTP module

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const tutorial = require('./tutorial')
-console.log(tutorial)
-console.log(tutorial.PI)
-console.log(tutorial.sum(1, 2))
-console.log(new tutorial.SomeMathObject())
+//create a simple
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     res.write('Hello World from nodejs')
+//     res.end()
+// })
+
+// server.listen(3000)
+
+const http = require('http')
+const server = http.createServer((req, res) => {
+    if(req.url === '/')
+    {
+        res.write('Hello world from nodejs')
+        res.end()
+    }
+    else{
+        res.write('using another domain')
+        res.end()
+    }
+})
+
+server.listen(3000)
