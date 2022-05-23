64aef5626b6f708c8923a59ccd87628764f1fbce
Vu_Huy <quochuy123dh@gmail.com>
2022-05-23 21:43:32 +0700

    serving static files with http and file system

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
+const http = require('http')
 const fs = require('fs')
-// const readStream = fs.createReadStream('./example.txt','utf-8')
-// const writeStream = fs.createWriteStream('example2.txt')
 
-//write file
-// readStream.pipe(writeStream)
-
-//compress file
-const zlib = require('zlib')
-// const gzip = zlib.createGzip()
-
-// const readStream = fs.createReadStream('./example.txt', 'utf-8')
-// const writeStream = fs.createWriteStream('compressed.zip')
-// readStream.pipe(gzip).pipe(writeStream)
-
-//uncompress file
-const gunzip = zlib.createGunzip()
-const readStream = fs.createReadStream('./compressed.zip')
-const writeStream = fs.createWriteStream('uncompressed.txt')
-readStream.pipe(gunzip).pipe(writeStream)
+//create a folder static contains files.
+http.createServer((req, res) => {
+    const readStream = fs.createReadStream('./static/example.json')
+    res.writeHead(200,{'Content-type': 'text/html'})
+    readStream.pipe(res)
+}).listen(3000)
