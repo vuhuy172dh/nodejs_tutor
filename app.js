a40a8df7017fc466464ddb77cd542d6b2c3c063e
Vu_Huy <quochuy123dh@gmail.com>
2022-05-24 20:30:40 +0700

    updated

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
 const app = express()
 app.use('/public',express.static(path.join(__dirname,'static')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'static','index.html'))
 })
-
-// app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/example', (req,res) => {
     res.send('hitting example route')
@@ -17,12 +14,6 @@ app.get('/example/:name/:age', (req, res) => {
     console.log(req.params)
     console.log(req.query)
     res.send('example with route params')
-})
-
-app.post('/', (req,res) => {
-    console.log(req.body)
-    //database work here
-    res.send('successfully posted data')
 })
 
 app.listen(3000)
