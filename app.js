4109383974affed0ffeb49ba3916f2dbbc5054e7
Vu_Huy <quochuy123dh@gmail.com>
2022-05-24 20:40:06 +0700

    http post request with express

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const tutorial = require('./tutorial')
-console.log(tutorial)
-console.log(tutorial.PI)
-console.log(tutorial.sum(1, 2))
-console.log(new tutorial.SomeMathObject())
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+app.use('/public',express.static(path.join(__dirname,'static')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'static','index.html'))
+})
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.get('/example', (req,res) => {
+    res.send('hitting example route')
+})
+
+app.get('/example/:name/:age', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send('example with route params')
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+    //database work here
+    res.send('successfully request')
+})
+
+app.listen(3000)
