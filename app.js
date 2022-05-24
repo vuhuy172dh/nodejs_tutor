fe3557c61b68f35ca6ad9b9b889e3824e42dab84
Vu_Huy <quochuy123dh@gmail.com>
2022-05-24 20:24:08 +0700

    serving static file with express

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
-
+app.use('/public',express.static(path.join(__dirname,'static')))
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.sendFile(path.join(__dirname,'static','index.html'))
 })
+
+// app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/example', (req,res) => {
     res.send('hitting example route')
@@ -13,6 +17,12 @@ app.get('/example/:name/:age', (req, res) => {
     console.log(req.params)
     console.log(req.query)
     res.send('example with route params')
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+    //database work here
+    res.send('successfully posted data')
 })
 
 app.listen(3000)
