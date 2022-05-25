ae2690ffda664e7db256ecd589cfa93464781ded
Vu_Huy <quochuy123dh@gmail.com>
2022-05-25 23:23:59 +0700

    user input validation with express and joi

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const Joi = require('joi')
 const app = express()
 app.use('/public',express.static(path.join(__dirname,'static')))
+app.use(express.urlencoded({extended:false}))
+//app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'static','index.html'))
-})
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-
-
-app.get('/example', (req,res) => {
-    res.send('hitting example route')
-})
-
-app.get('/example/:name/:age', (req, res) => {
-    console.log(req.params)
-    console.log(req.query)
-    res.send('example with route params')
 })
 
 app.post('/', (req,res) => {
     console.log(req.body)
-    //database work here
-    res.json({success: true})
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password : Joi.string().min(5).max(10).required()
+    })
+    const result = schema.validate(req.body)
+    if(result.error){
+        console.log(result.error)
+        res.send("successfully posted data")
+    }
+    else{
+        console.log(result.value)
+        res.send("successfully posted data")
+    }
 })
 
 app.listen(3000)
